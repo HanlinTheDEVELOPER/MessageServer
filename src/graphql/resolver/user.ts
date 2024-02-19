d13fdc100b69b0e2909a19af0a20c2d00e2e9f65
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-02-19 14:08:55 +0630

    se fn

--- src/graphql/resolver/user.ts
+++ src/graphql/resolver/user.ts
@@ -1,10 +1,41 @@
 import { GraphQlContext } from "../../types/types";
 import { CreateUsernameResponse } from "../../types/types";
+import { GraphQLError } from "graphql";
 
 const resolvers = {
   Query: {
-    searchUsers: (args: { username: string }) => {
-      console.log("Inside server");
+    searchUsers: async (
+      _: any,
+      args: { username: string },
+      context: GraphQlContext
+    ) => {
+      const { session, prisma } = context;
+
+      if (!session) {
+        throw new GraphQLError("Not Authorize");
+      }
+
+      const {
+        user: { username: myUsername },
+      } = session;
+
+      const { username: searchedUsername } = args;
+
+      try {
+        const users = await prisma.user.findMany({
+          where: {
+            username: {
+              contains: searchedUsername,
+              not: myUsername,
+              mode: "insensitive",
+            },
+          },
+        });
+        return users;
+      } catch (error) {
+        console.log(error);
+        throw new GraphQLError(error?.message);
+      }
     },
   },
   Mutation: {
